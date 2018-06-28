2f3d6d27b2b1ea95de6e3c4e52d3f0307a4dffe4
Muhammad Zia Shahid <ziagaggoo@gmail.com>
2018-06-28 01:43:50 -0700

    Initialize landing activity
    Butter knife library added
    views in landing acitivty initialized
    listeners added

--- app/src/main/java/com/example/videoplayerwithteams/activities/LandingActivity.java
+++ app/src/main/java/com/example/videoplayerwithteams/activities/LandingActivity.java
@@ -1,17 +1,74 @@
 package com.example.videoplayerwithteams.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.videoplayerwithteams.R;
+import com.example.videoplayerwithteams.constants.Constants;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LandingActivity extends AppCompatActivity
 {
+    @BindView(R.id.btnPlayVideo)
+    Button btnPlayVideo;
+    
+    @BindView(R.id.seekBarPlayVideoFrom)
+    SeekBar seekBarPlayVideoFrom;
+    
+    @BindView(R.id.tvMilliSeconds)
+    TextView tvMilliSeconds;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        ButterKnife.bind(this);
+        bindViews();
+    }
+    
+    private void bindViews()
+    {
+        seekBarPlayVideoFrom.setMax(Constants.VIDEO_LENGTH_IN_SECONDS);
+        seekBarPlayVideoFrom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                tvMilliSeconds.setText((i) + "");
+            }
+            
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+                
+            }
+            
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+                
+            }
+        });
+        
+        btnPlayVideo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intentToMainActivity = new Intent(LandingActivity.this, MainActivity.class);
+                intentToMainActivity.putExtra(Constants.VIDEO_SEEK_TIME_KEY, Integer.parseInt(tvMilliSeconds.getText().toString()));
+                startActivity(intentToMainActivity);
+                finish();
+            }
+        });
+        
     }
 }
---
+++ app/src/main/java/com/example/videoplayerwithteams/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.example.videoplayerwithteams.constants;
+
+/**
+ * Created by ZeeZee on 6/28/2018.
+ */
+
+public interface Constants
+{
+    int VIDEO_LENGTH_IN_SECONDS = (11 * 60) + 35;
+    String VIDEO_SEEK_TIME_KEY = "VIDEO_SEEK_TIME_KEY";
+}
